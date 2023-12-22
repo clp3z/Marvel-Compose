9e673e582a7bbb45a2dd4f60ef11409a3b60b971
Clelia <clelia.clp3z@gmail.com>
2023-12-22 01:27:16 +0100

    Passing data implicitly with CompositionLocal

--- app/src/main/java/com/clp3z/marvelcompose/ui/views/DrawerItem.kt
+++ app/src/main/java/com/clp3z/marvelcompose/ui/views/DrawerItem.kt
@@ -12,12 +12,17 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.clp3z.marvelcompose.ui.MarvelScreen
@@ -31,41 +36,37 @@ fun DrawerItem(
 ) {
     val title = stringResource(id = item.title)
     val materialColors = MaterialTheme.colors
-    Box {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp, 4.dp)
-                .clickable { onDrawerItemClicked(item) }
-                .clip(RoundedCornerShape(4.dp))
-                .background(
-                    when {
-                        isSelected -> materialColors.primary.copy(alpha = 0.12f)
-                        else -> materialColors.surface
-                    }
-                )
-                .padding(12.dp)
+    val localContentColor = if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface
 
-        ) {
-            Icon(
-                imageVector = item.icon,
-                contentDescription = "$title icon",
-                tint = when (isSelected) {
-                    true -> materialColors.primary
-                    else -> materialColors.onSurface.copy(
-                        alpha = ContentAlpha.medium
+    CompositionLocalProvider(
+        LocalTextStyle provides MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
+        LocalContentColor provides localContentColor
+    ) {
+        Box {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp, 4.dp)
+                    .clickable { onDrawerItemClicked(item) }
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(
+                        when (isSelected) {
+                            true -> LocalContentColor.current.copy(alpha = 0.12f)
+                            else -> materialColors.surface
+                        }
+                    )
+                    .padding(12.dp)
+
+            ) {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = "$title icon"
                     )
                 }
-            )
-            Spacer(Modifier.width(16.dp))
-            Text(
-                text = title,
-                style = MaterialTheme.typography.body1,
-                color = when (isSelected) {
-                    true -> materialColors.primary
-                    else -> materialColors.onSurface
-                }
-            )
+                Spacer(Modifier.width(16.dp))
+                Text(text = title)
+            }
         }
     }
 }
